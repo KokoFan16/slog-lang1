6256f9484642ab3afaf0a39ecdb3f0bcfd7a3018
kokofan <koko14fan@gmail.com>
2024-08-27 13:35:46 -0500

    branch tuna

--- backend/src/comm/all_to_allv_comm.cpp
+++ backend/src/comm/all_to_allv_comm.cpp
@@ -7,7 +7,8 @@
 #include "../parallel_RA_inc.h"
 #include <unistd.h>
 
-
+int twophase_rbruck_alltoallv(int r, char *sendbuf, int *sendcounts, int *sdispls, MPI_Datatype sendtype,
+		char *recvbuf, int *recvcounts, int *rdispls, MPI_Datatype recvtype, MPI_Comm comm);
 
 void all_to_all_comm(vector_buffer* vectorized_send_buffer, int vectorized_send_buffer_size, int* send_counts, int *recv_buffer_size, u64 **recv_buffer, MPI_Comm comm)
 {
@@ -52,7 +53,9 @@ void all_to_all_comm(vector_buffer* vectorized_send_buffer, int vectorized_send_
     *recv_buffer = new u64[*recv_buffer_size];
 
     /// Actual data transfer
-    MPI_Alltoallv(send_buffer, send_counts, send_displacements, MPI_UNSIGNED_LONG_LONG, *recv_buffer, recv_counts, recv_displacements, MPI_UNSIGNED_LONG_LONG, comm);
+    int r = ceil(sqrt(nprocs));
+    twophase_rbruck_alltoallv(r, send_buffer, send_counts, send_displacements, MPI_UNSIGNED_LONG_LONG, *recv_buffer, recv_counts, recv_displacements, MPI_UNSIGNED_LONG_LONG, comm);
+//    MPI_Alltoallv(send_buffer, send_counts, send_displacements, MPI_UNSIGNED_LONG_LONG, *recv_buffer, recv_counts, recv_displacements, MPI_UNSIGNED_LONG_LONG, comm);
 
     /// cleanup
     delete[] recv_counts;
@@ -69,8 +72,7 @@ void comm_compaction_all_to_all(all_to_allv_buffer compute_buffer, int **recv_bu
 {
     u32 RA_count = compute_buffer.ra_count;
     int nprocs = compute_buffer.nprocs;
-    int rank;
-    MPI_Comm_rank(comm, &rank);
+    int r = ceil(sqrt(nprocs));
 
     *recv_buffer_counts = new int[RA_count * nprocs];
     memset(*recv_buffer_counts, 0, RA_count * nprocs * sizeof(int));
@@ -130,7 +132,8 @@ void comm_compaction_all_to_all(all_to_allv_buffer compute_buffer, int **recv_bu
     MPI_Allreduce(&local_max_count, &max_send_count, 1, MPI_INT, MPI_MAX, comm);
     MPI_Allreduce(&local_max_count, &average_send_count, 1, MPI_INT, MPI_SUM, comm);
 
-    MPI_Alltoallv(send_buffer, compute_buffer.cumulative_tuple_process_map, send_disp, MPI_UNSIGNED_LONG_LONG, *recv_buffer, recv_counts, recv_displacements, MPI_UNSIGNED_LONG_LONG, comm);
+    twophase_rbruck_alltoallv(r, send_buffer, compute_buffer.cumulative_tuple_process_map, send_disp, MPI_UNSIGNED_LONG_LONG, *recv_buffer, recv_counts, recv_displacements, MPI_UNSIGNED_LONG_LONG, comm);
+//    MPI_Alltoallv(send_buffer, compute_buffer.cumulative_tuple_process_map, send_disp, MPI_UNSIGNED_LONG_LONG, *recv_buffer, recv_counts, recv_displacements, MPI_UNSIGNED_LONG_LONG, comm);
 
     delete[] send_buffer;
     delete[] send_disp;
@@ -138,5 +141,136 @@ void comm_compaction_all_to_all(all_to_allv_buffer compute_buffer, int **recv_bu
     delete[] recv_counts;
 }
 
+
+static int myPow(int x, unsigned int p) {
+  if (p == 0) return 1;
+  if (p == 1) return x;
+
+  int tmp = myPow(x, p/2);
+  if (p%2 == 0) return tmp * tmp;
+  else return x * tmp * tmp;
+}
+
+int twophase_rbruck_alltoallv(int r, char *sendbuf, int *sendcounts, int *sdispls, MPI_Datatype sendtype,
+		char *recvbuf, int *recvcounts, int *rdispls, MPI_Datatype recvtype, MPI_Comm comm){
+
+	if ( r < 2 ) { return -1; }
+
+	int rank, nprocs, typesize;
+	MPI_Comm_rank(comm, &rank);
+	MPI_Comm_size(comm, &nprocs);
+
+	if ( r > nprocs ) { r = nprocs; }
+
+	char* sendcopy;
+	if (!sendbuf) {
+		int total_count = 0;
+		for (int i = 0; i < nprocs; i++) { total_count += sendcounts[i]; }
+		sendcopy = (char*) malloc(total_count*typesize);
+		memcpy(sendcopy, recvbuf, total_count*typesize);
+	}
+	else { sendcopy = sendbuf; }
+
+	int w, nlpow, d;
+	int local_max_count=0, max_send_count=0;
+	int sendNcopy[nprocs], rotate_index_array[nprocs], pos_status[nprocs];
+
+	MPI_Type_size(sendtype, &typesize);
+
+	w = ceil(log(nprocs) / log(r)); // calculate the number of digits when using r-representation
+	nlpow = myPow(r, w-1); // maximum send number of elements
+	d = (myPow(r, w) - nprocs) / nlpow; // calculate the number of highest digits
+
+	// 1. Find max send count
+	for (int i = 0; i < nprocs; i++) {
+		if (sendcounts[i] > local_max_count)
+			local_max_count = sendcounts[i];
+	}
+	MPI_Allreduce(&local_max_count, &max_send_count, 1, MPI_INT, MPI_MAX, comm);
+	memcpy(sendNcopy, sendcounts, nprocs*sizeof(int));
+
+    // 2. create local index array after rotation
+	for (int i = 0; i < nprocs; i++)
+		rotate_index_array[i] = (2*rank-i+nprocs)%nprocs;
+
+	// 3. exchange data with log(P) steps
+	char* extra_buffer = (char*) malloc(max_send_count*typesize*nprocs);
+	char* temp_send_buffer = (char*) malloc(max_send_count*typesize*nlpow);
+	char* temp_recv_buffer = (char*) malloc(max_send_count*typesize*nlpow);
+	memset(pos_status, 0, nprocs*sizeof(int));
+
+	int sent_blocks[nlpow];
+	int di = 0, spoint = 1, distance = myPow(r, w-1), next_distance = distance*r;
+
+	for (int x = w-1; x > -1; x--) {
+		int ze = (x == w - 1)? r - d: r;
+		for (int z = ze-1; z > 0; z--) {
+
+			// 1) get the sent data-blocks
+			di = 0;
+			spoint = z * distance;
+			for (int i = spoint; i < nprocs; i += next_distance) {
+				for (int j = i; j < (i+distance); j++) {
+					if (j > nprocs - 1 ) { break; }
+					int id = (j + rank) % nprocs;
+					sent_blocks[di++] = id;
+				}
+			}
+
+			// 2) prepare metadata and send buffer
+			int metadata_send[di];
+			int sendCount = 0, offset = 0;
+			for (int i = 0; i < di; i++) {
+				int send_index = rotate_index_array[sent_blocks[i]];
+				metadata_send[i] = sendNcopy[send_index];
+				if (pos_status[send_index] == 0)
+					memcpy(&temp_send_buffer[offset], &sendcopy[sdispls[send_index]*typesize], sendNcopy[send_index]*typesize);
+				else
+					memcpy(&temp_send_buffer[offset], &extra_buffer[sent_blocks[i]*max_send_count*typesize], sendNcopy[send_index]*typesize);
+				offset += sendNcopy[send_index]*typesize;
+			}
+
+			// 3) exchange metadata
+			int recvrank = (rank + spoint) % nprocs; // receive data from rank - 2^step process
+			int sendrank = (rank - spoint + nprocs) % nprocs; // send data from rank + 2^k process
+
+			int metadata_recv[di];
+			MPI_Sendrecv(metadata_send, di, MPI_INT, sendrank, 0, metadata_recv, di, MPI_INT, recvrank, 0, comm, MPI_STATUS_IGNORE);
+
+			for(int i = 0; i < di; i++)
+				sendCount += metadata_recv[i];
+
+			// 4) exchange data
+			MPI_Sendrecv(temp_send_buffer, offset, MPI_CHAR, sendrank, 1, temp_recv_buffer, sendCount*typesize, MPI_CHAR, recvrank, 1, comm, MPI_STATUS_IGNORE);
+
+			// 5) replaces
+			offset = 0;
+			for (int i = 0; i < di; i++) {
+				int send_index = rotate_index_array[sent_blocks[i]];
+
+				int origin_index = (sent_blocks[i] - rank + nprocs) % nprocs;
+				if (origin_index % next_distance == (recvrank - rank + nprocs) % nprocs)
+					memcpy(&recvbuf[rdispls[sent_blocks[i]]*typesize], &temp_recv_buffer[offset], metadata_recv[i]*typesize);
+				else
+					memcpy(&extra_buffer[sent_blocks[i]*max_send_count*typesize], &temp_recv_buffer[offset], metadata_recv[i]*typesize);
+
+				offset += metadata_recv[i]*typesize;
+				pos_status[send_index] = 1;
+				sendNcopy[send_index] = metadata_recv[i];
+			}
+		}
+		distance /= r;
+		next_distance /= r;
+	}
+
+	memcpy(&recvbuf[rdispls[rank]*typesize], &sendcopy[sdispls[rank]*typesize], recvcounts[rank]*typesize);
+
+	free(sendcopy);
+	free(temp_send_buffer);
+	free(temp_recv_buffer);
+	free(extra_buffer);
+
+	return 0;
+}
 
 
